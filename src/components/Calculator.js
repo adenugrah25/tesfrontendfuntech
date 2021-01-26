4d1900d6fbb0dc68251eb985700d32997daf216d
adenugrah25 <adenugrah25@gmail.com>
2021-01-26 22:57:10 +0700

    change the code

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,4 @@
 import React from "react";
-let a = 1
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -7,51 +6,171 @@ class Calculator extends React.Component {
     this.state = {
       fcalcone: null,
       fcalctwo: null,
-      fcalcthree: null, 
+      fcalcthree: null,
       hasil: 0,
     };
   }
 
   inputOnChange = (event) => {
-     this.setState({
-        [event.target.name]: event.target.value,
-     });
+    this.setState({
+      [event.target.name]:
+        event.target.value === "" ? null : parseFloat(event.target.value),
+    });
   };
 
   checkBoxOnChange = (event) => {
-    if (event.target.checked) {
-        this.setState({
-            [event.target.id]: event.target.value
-        })
-    }  
+    console.log(event.target.value);
+    if (!event.target.checked) {
+      this.setState({
+        [event.target.id]: event.target.value,
+      });
+    }
   };
 
   calculate = (event) => {
     var res = null;
-    const firstValue = this.state.fcalcone != null ? parseFloat(this.state.fcalcone) : null;
-    const secondValue = this.state.fcalctwo != null ? parseFloat(this.state.fcalctwo) : null;
-    const thirdValue = this.state.fcalcthree != null ? parseFloat(this.state.fcalcthree) : null;
-     
+
+    const arr = [
+      this.state.fcalcone,
+      this.state.fcalctwo,
+      this.state.fcalcthree,
+    ];
+    var validateArr = arr.filter((x) => x !== null).length;
+
+    if (validateArr === 1) {
+      window.alert("Input tidak boleh hanya satu");
+      console.error("Input tidak boleh hanya satu");
+    }
 
     switch (event.target.value) {
       case "+":
-        res = firstValue + secondValue + thirdValue;
+        res = this.sum();
         break;
       case "-":
-        res = firstValue - secondValue - thirdValue;
+        res = this.subtraction();
         break;
       case "*":
-        res = firstValue * secondValue * thirdValue;
+        res = this.multiplication();
         break;
       case "/":
-        res = firstValue / secondValue / thirdValue;
+        res = this.division();
         break;
     }
 
     this.setState({
-      hasil: res
+      hasil: res,
     });
+  };
 
+  sum = () => {
+    var result = 0;
+
+    const firstValue =
+      this.state.fcalcone == null || this.state.fcalcone === ""
+        ? null
+        : this.state.fcalcone;
+    const secondValue =
+      this.state.fcalctwo == null || this.state.fcalctwo === ""
+        ? null
+        : this.state.fcalctwo;
+    const thirdValue =
+      this.state.fcalcthree == null || this.state.fcalcthree === ""
+        ? null
+        : this.state.fcalcthree;
+
+    console.log(this.state.fcalcone);
+    console.log(this.state.fcalctwo);
+    console.log(this.state.fcalcthree);
+
+    result = firstValue + secondValue + thirdValue;
+
+    console.log(result);
+    return result;
+  };
+
+  subtraction = () => {
+    var result = 0;
+
+    const firstValue =
+      this.state.fcalcone == null || this.state.fcalcone === ""
+        ? null
+        : this.state.fcalcone;
+    const secondValue =
+      this.state.fcalctwo == null || this.state.fcalctwo === ""
+        ? null
+        : this.state.fcalctwo;
+    const thirdValue =
+      this.state.fcalcthree == null || this.state.fcalcthree === ""
+        ? null
+        : this.state.fcalcthree;
+
+    if (firstValue === null) {
+      result = secondValue - thirdValue;
+    } else {
+      result = firstValue - secondValue - thirdValue;
+    }
+
+    console.log(this.state.fcalcone);
+    console.log(this.state.fcalctwo);
+    console.log(this.state.fcalcthree);
+
+    console.log(result);
+    return result;
+  };
+
+  multiplication = () => {
+    var result = 0;
+
+    const firstValue =
+      this.state.fcalcone == null || this.state.fcalcone === ""
+        ? 1
+        : this.state.fcalcone;
+    const secondValue =
+      this.state.fcalctwo == null || this.state.fcalctwo === ""
+        ? 1
+        : this.state.fcalctwo;
+    const thirdValue =
+      this.state.fcalcthree == null || this.state.fcalcthree === ""
+        ? 1
+        : this.state.fcalcthree;
+
+    console.log(this.state.fcalcone);
+    console.log(this.state.fcalctwo);
+    console.log(this.state.fcalcthree);
+
+    result = firstValue * secondValue * thirdValue;
+
+    console.log(result);
+    return result;
+  };
+
+  division = () => {
+    var result = 0;
+    const firstValue =
+      this.state.fcalcone == null || this.state.fcalcone === ""
+        ? 1
+        : this.state.fcalcone;
+    const secondValue =
+      this.state.fcalctwo == null || this.state.fcalctwo === ""
+        ? 1
+        : this.state.fcalctwo;
+    const thirdValue =
+      this.state.fcalcthree == null || this.state.fcalcthree === ""
+        ? 1
+        : this.state.fcalcthree;
+
+    if (firstValue === 1) {
+      result = secondValue / thirdValue;
+    } else {
+      result = firstValue / secondValue / thirdValue;
+    }
+
+    console.log(this.state.fcalcone);
+    console.log(this.state.fcalctwo);
+    console.log(this.state.fcalcthree);
+
+    console.log(result);
+    return result;
   };
 
   render() {
@@ -61,18 +180,45 @@ class Calculator extends React.Component {
           {" "}
           <h1>Calculator</h1> <br></br>
           <p>
-            <input type="number" name="fcalcone" onChange={this.inputOnChange} />{" "}
-            <input type="checkbox" id="fcalcone" onChange={(e) => this.checkBoxOnChange(e)} value={this.state.fcalcone} ></input>
+            <input
+              type="number"
+              name="fcalcone"
+              onChange={this.inputOnChange}
+            />{" "}
+            <input
+              type="checkbox"
+              id="fcalcone"
+              onChange={(e) => this.checkBoxOnChange(e)}
+              value={this.state.fcalcone}
+            ></input>
           </p>{" "}
           <br></br>
           <p>
-            <input type="number" name="fcalctwo" onChange={this.inputOnChange} />{" "}
-            <input type="checkbox" id="fcalctwo" onChange={(e) => this.checkBoxOnChange(e)} value={this.state.fcalctwo} ></input>
+            <input
+              type="number"
+              name="fcalctwo"
+              onChange={this.inputOnChange}
+            />{" "}
+            <input
+              type="checkbox"
+              id="fcalctwo"
+              onChange={(e) => this.checkBoxOnChange(e)}
+              value={this.state.fcalctwo}
+            ></input>
           </p>{" "}
           <br></br>
           <p>
-            <input type="number" name="fcalcthree" onChange={this.inputOnChange} />{" "}
-            <input type="checkbox" id="fcalcthree" onChange={(e) => this.checkBoxOnChange(e)} value={this.state.fcalcthree} ></input>
+            <input
+              type="number"
+              name="fcalcthree"
+              onChange={this.inputOnChange}
+            />{" "}
+            <input
+              type="checkbox"
+              id="fcalcthree"
+              onChange={(e) => this.checkBoxOnChange(e)}
+              value={this.state.fcalcthree}
+            ></input>
           </p>{" "}
           <br></br>
           <input type="button" onClick={this.calculate} value="+"></input>
